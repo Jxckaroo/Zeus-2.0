095315135ea1d5d64c8458e12f3e51c2bb7a2640
Jack Mollart <jack.mollart@paceit.co.uk>
2019-05-05 17:35:40 +0100

    Update the application status upon accepting/declining.

--- commands/admin/accept_application.js
+++ commands/admin/accept_application.js
@@ -1,5 +1,12 @@
 const Commando = require("discord.js-commando");
 const Config = require("../../configs/config.json");
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: Config.DB.HOST,
+    user: Config.DB.USER,
+    password: Config.DB.PASSWORD,
+    database: Config.DB.DATABASE
+});
 const Admins = Config.ADMINS;
 
 class AcceptApplicationCommand extends Commando.Command {
@@ -36,6 +43,17 @@ class AcceptApplicationCommand extends Commando.Command {
 
         message.channel.send("I have accepted that user.");
         member.send("Congratulations, you are now a full member at " + Config.DISCORD_NAME + "!");
+
+        // Update application status
+        let response = await result.forEach(async(element, i) => {
+                await con.query('UPDATE applications SET status = ?', "Accepted", async function(err, result) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log(result);
+                    }
+                });
+        });
     }
 }
 
--- commands/admin/decline_application.js
+++ commands/admin/decline_application.js
@@ -1,5 +1,12 @@
 const Commando = require("discord.js-commando");
 const Config = require("../../configs/config.json");
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: Config.DB.HOST,
+    user: Config.DB.USER,
+    password: Config.DB.PASSWORD,
+    database: Config.DB.DATABASE
+});
 const Admins = Config.ADMINS;
 
 class DeclineApplicationCommand extends Commando.Command {
@@ -36,6 +43,17 @@ class DeclineApplicationCommand extends Commando.Command {
             " at " + Config.DISCORD_NAME + ". We have removed your recruit role at this time." +
             " If you would like to re-apply, simply submit another application and" +
             " stick to the guidelines to become a member.");
+
+        // Update application status
+        let response = await result.forEach(async(element, i) => {
+            await con.query('UPDATE applications SET status = ?', "Declined", async function(err, result) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(result);
+                }
+            });
+        });
     }
 }
 
